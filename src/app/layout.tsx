f36b63dfa54022b6bbdbeb9920c74c449f9adce1
Barbara Oliveira <barbaragdeoliveira@hotmail.com>
2026-01-04 21:39:53 -0300

    refac: The layout page has been redesigned to add the toast to the other pages.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // ✅ importante
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+        <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar />
+      </body>
     </html>
   );
 }
